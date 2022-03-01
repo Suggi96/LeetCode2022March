fa442d890b7d4fe959c7ace55baf209337a193fb
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-01 11:45:24 +0530

    Time: 35 ms (59.26%), Space: 22.3 MB (55.92%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -3,26 +3,18 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int low = 0;
         int high = nums.size()-1;
-        //boundary cases
         if(high==0)
             return nums[0];
-        //if first element is unique return it
         if(nums[low]!=nums[low+1])
             return nums[low];
-        //if last element is unique return it
+
         if(nums[high]!=nums[high-1])
             return nums[high];
         
-        //binary search
         while(low<=high) {
             int mid = low + (high-low)/2;
-            //check if mid is unique, if yes return it
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
                 return nums[mid];
-            //make decision by checking if mid index value
-            //if mid is even then nums[mid]==nums[mid+1]
-            //if mid is odd then nums[mid]==nums[mid-1]
-            //if any of above condition is satisfied move low
             if(mid%2==0 && nums[mid]==nums[mid+1] ||
               mid%2==1 && nums[mid]==nums[mid-1]) {
                 low = mid + 1;
