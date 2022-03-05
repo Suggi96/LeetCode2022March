db951fdde255df2047ff218b6b0ca64bb2d41921
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-05 17:10:15 +0530

    Time: 15 ms (39.06%), Space: 13.9 MB (15.60%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> cnt(10002);
-		for(int i=0;i<nums.size();i++)
-		{
-			cnt[nums[i]]+=nums[i];
-		}
-
-		vector<int> dp(10002);
-		dp[1]=cnt[1];
-		dp[2]=max(cnt[1],cnt[2]);
-		for(int i=3;i<dp.size();i++)
-		{
-			dp[i]=max(dp[i-2]+cnt[i],dp[i-1]);
-		}
-
-		return max(dp[10000],dp[9999]);
+        int n = 10002;
+        vector<int> dp(n, 0);
+        vector<int> freq(n, 0);
+        for(int i=0;i<nums.size();i++) {
+            freq[nums[i]]++;
+        }
+        dp[0] = 0;
+        dp[1] = 1 * freq[1];
+        
+        for(int i=2;i<10002;i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + i*freq[i]);
+        }
+        return dp[n-1];
     }
 };
