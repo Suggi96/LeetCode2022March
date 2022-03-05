2838ce1119213dafdbfecf0e642e23be85e50a14
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-05 09:31:31 +0530

    Time: 5 ms (82.47%), Space: 6 MB (91.24%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double mat[101][101] = {0.0};
+        mat[0][0] = poured;
+        for(int i=0;i<100;i++) {
+            for(int j=0;j<=i;j++) {
+                if(mat[i][j]>=1) {
+                    //left cup
+                    mat[i+1][j] += (mat[i][j] - 1.0) / 2.0;
+                    //right cup 
+                    mat[i+1][j+1] += (mat[i][j] - 1.0) / 2.0;
+                    mat[i][j] = 1;
+                } 
+            }
+        }
+        return mat[query_row][query_glass];
+    }
+};
