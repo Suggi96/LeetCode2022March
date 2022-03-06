0c03ac1044625951ea154c855f9b0c124923c5ac
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-06 18:21:25 +0530

    Time: 11 ms (75.00%), Space: 12.7 MB (8.33%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
-        unordered_map<int, int> mp;
+        map<int, int> mp;
         for(int i=0;i<nums.size()-1;i++) {
             if(nums[i]==key) {
                 mp[nums[i+1]]++;
