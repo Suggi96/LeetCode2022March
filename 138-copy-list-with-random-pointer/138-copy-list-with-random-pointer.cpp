1eea90bb31813ffebcdb059ef7c2638c87af6aef
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-12 11:46:17 +0530

    Time: 27 ms (5.95%), Space: 11.5 MB (8.60%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,36 +1,38 @@
 /*
 // Definition for a Node.
 class Node {
+public:
     int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
-}
+};
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
-        Node dummy = new Node(-1);
-        Node cur = head, copy = dummy;
-        Map<Node, Node> map = new HashMap();
-        while(cur!=null) {
-            copy.next = new Node(cur.val);
-            copy = copy.next;
-            map.put(cur, copy);
-            cur = cur.next;
+public:
+    Node* copyRandomList(Node* head) {
+        Node* dummy = new Node(-1);
+        Node* cur = head, *copy = dummy;
+        map<Node*, Node*> mp;
+        while(cur!=NULL) {
+            copy->next = new Node(cur->val);
+            copy = copy->next;
+            mp[cur] = copy;
+            cur = cur->next;
         }
         cur = head;
-        copy = dummy.next;
-        while(cur!=null) {
-            copy.random = map.get(cur.random);
-            copy = copy.next;
-            cur = cur.next;
+        copy = dummy->next;
+        while(cur!=NULL) {
+            copy->random = mp[cur->random];
+            copy = copy->next;
+            cur = cur->next;
         }
-        return dummy.next;
+        return dummy->next;
     }
-}
+};
