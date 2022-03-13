1ca339f8fb3324b5e50205bc1d7be6155693a8ac
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-13 22:03:00 +0530

    Time: 783 ms (26.09%), Space: 164.7 MB (8.70%) - LeetHub

--- 2201-count-artifacts-that-can-be-extracted/2201-count-artifacts-that-can-be-extracted.java
+++ 2201-count-artifacts-that-can-be-extracted/2201-count-artifacts-that-can-be-extracted.java
@@ -1,24 +1,31 @@
 class Solution {
-    public int digArtifacts(int N, int[][] artifacts, int[][] dig) {
-        int[][] mat = new int[N][N];
-        int cur = 0;
-        for(int[] artif : artifacts)
-        {
-            ++cur;
-            for(int i = artif[0]; i <= artif[2]; i++)
-                for(int j = artif[1]; j <= artif[3]; j++)
-                        mat[i][j] = cur; // Marking each artifact with a unique number
+public:
+    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
+        vector<vector<bool>>visited(n,vector<bool>(n,0));
+        for(auto vec:dig){
+            visited[vec[0]][vec[1]] = 1;
         }
         
-        for(int i = 0; i < dig.length; i++)
-            mat[dig[i][0]][dig[i][1]] = -1; // Marking excavated
+        int count = 0;
+        for(auto artifact:artifacts){
+            int r1 = artifact[0];
+            int c1 = artifact[1];
+            int r2 = artifact[2];
+            int c2 = artifact[3];
+            bool flag = true;
+			
+            for(int i = r1;i<=r2;i++){
+                for(int j = c1;j<=c2;j++){
+                    if(!visited[i][j]){
+                        flag = false;
+                    }
+                }
+            }
+            
+            if(flag)
+                count++;
+        }
         
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < N; j++)
-                if(mat[i][j] > 0)
-                    set.add(mat[i][j]); // All the unexcavated artifacts
-        
-        return cur-set.size();
+        return count;
     }
-}
+};
