fcdc2a77e6bc1ee877d63bbd3269e26d933132ee
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-13 17:12:16 +0530

    Time: 3 ms (100.00%), Space: 10.6 MB (30.00%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int> jindex;
-        for(int j=0;j<nums.size();j++) {
-            if(key==nums[j])
-                jindex.push_back(j);
+     int n = nums.size();
+        
+        queue<int> q;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == key) q.push(i);
         }
-        set<int> s;
-        for(int i=0;i<nums.size();i++) {
-            for(int j: jindex) {
-                if(abs(i-j)<=k && nums[j]==key)
-                    s.insert(i);
+        
+        vector<int> ans;
+        for(int i=0; i<n; i++) {
+            if(q.empty()) break;
+            int idx = q.front();
+            
+            if(abs(idx - i) <= k) {
+                ans.push_back(i);
+            }
+            else {
+                if(i > idx) {
+					// if not able to cover current index then surely can't cover index after it
+                    q.pop();
+					
+					// lets again try for current index
+                    i--;
+                }
             }
         }
-        vector<int> ans;
-        for(int i: s) {
-            ans.push_back(i);
-        }
-        sort(ans.begin(), ans.end());
+        
         return ans;
     }
 };
