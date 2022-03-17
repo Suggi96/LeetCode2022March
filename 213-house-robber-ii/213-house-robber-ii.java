8a809e2938cfddf45ad8e6bc17b5eb721f1830cd
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-17 18:31:02 +0530

    Time: 1 ms (30.73%), Space: 42.6 MB (5.24%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -7,11 +7,11 @@ class Solution {
         int n = nums.length;
         if(n==1) //if single house is present return  nums[0] as ans
             return nums[0];
-        int ans1 = maxMoney(0, nums, n-2, new HashMap<Integer, Integer>());
-        int ans2 = maxMoney(1, nums, n-1, new HashMap<Integer, Integer>());
+        int ans1 = maxMoney(0, n-2, nums, new HashMap<Integer, Integer>());
+        int ans2 = maxMoney(1, n-1, nums, new HashMap<Integer, Integer>());
         return Math.max(ans1, ans2);
     }
-    private int maxMoney(int curIndex, int[] nums,int lastIdx, 
+    private int maxMoney(int curIndex, int lastIdx, int[] nums,
                          HashMap<Integer, Integer> memo) {
         if(curIndex>lastIdx) {
             return 0;
@@ -21,8 +21,8 @@ class Solution {
         if(memo.containsKey(curKey))
             return memo.get(curKey);
         
-        int rob = nums[curIndex] + maxMoney(curIndex+2, nums, lastIdx, memo);
-        int norob = maxMoney(curIndex+1, nums, lastIdx, memo);
+        int rob = nums[curIndex] + maxMoney(curIndex+2,lastIdx, nums, memo);
+        int norob = maxMoney(curIndex+1,lastIdx, nums, memo);
         
         memo.put(curKey, Math.max(rob, norob));
         
