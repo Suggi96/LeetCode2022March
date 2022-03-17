dbbda7549d3c35f6cf924d834235cdb12a2d27e7
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-17 18:22:53 +0530

    Time: 3 ms (6.16%), Space: 41.1 MB (57.12%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -5,7 +5,7 @@ class Solution {
         //now problem gets converted to house robber 1
         //return max(ans1, ans2) 
         int n = nums.length;
-        if(n==1)
+        if(n==1) //if single house is present return  nums[0] as ans
             return nums[0];
         int ans1 = maxMoney(0, nums, n-2, new HashMap<Integer, Integer>());
         int ans2 = maxMoney(1, nums, n-1, new HashMap<Integer, Integer>());
