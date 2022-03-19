7451045ce00ad1369fc6aef85fa6ba8224b79b43
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-19 22:48:02 +0530

    Time: 82 ms (5.40%), Space: 71.2 MB (5.19%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,16 +1,24 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int[] freq = new int[10002];
-        int[] dp = new int[10002];
         for(int i=0;i<nums.length;i++) {
             freq[nums[i]]++;
         }
-        dp[0] = 0;
-        dp[1] = 1 * freq[1];
-        
-        for(int i=2;i<10002;i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + i*freq[i]);
+        HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+        return maxPoints(0, freq, memo); // 0-> cur idx
+    } 
+    private int maxPoints(int curIdx, int[] freq, HashMap<Integer, Integer> memo) {
+        if(curIdx >= freq.length) {
+            return 0;
         }
-        return dp[10001];
+        int curKey = curIdx;
+        if(memo.containsKey(curKey))
+            return memo.get(curKey);
+        
+        int del = curIdx*freq[curIdx] + maxPoints(curIdx+2, freq, memo);
+        int nodel = maxPoints(curIdx+1, freq, memo);
+        
+        memo.put(curKey, Math.max(del, nodel));
+        return memo.get(curKey);
     }
 }
