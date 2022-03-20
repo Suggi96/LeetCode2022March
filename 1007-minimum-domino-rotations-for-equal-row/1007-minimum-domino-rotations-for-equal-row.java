b8760a18272f4bdf5e60a6f71584a4c5be6c1f90
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-20 13:49:23 +0530

    Time: 62 ms (6.78%), Space: 109.8 MB (15.67%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -4,27 +4,20 @@ class Solution {
         HashMap<Integer, Integer> freqB = new HashMap<>();
         int maxFreq_element_T = maxFreq_StoreFreq(freqT, tops);
         int maxFreq_element_B = maxFreq_StoreFreq(freqB, bottoms);
-        
-        System.out.println("mfT "+maxFreq_element_T);
-        System.out.println("mfB "+maxFreq_element_B);
 
         //iterate through tops and bottoms , find swaps to make entire top or bottom equal
         //return min of both swaps as answer
         int swapsT = makeEqual(tops, bottoms, maxFreq_element_T);
         int swapsB = makeEqual(bottoms, tops, maxFreq_element_B);
         
-        System.out.println("swapT "+ swapsT);
-        System.out.println("swapB "+ swapsB);
         if(swapsT==0 || swapsB==0)
             return 0;
         
         if(swapsT==999 && swapsB==999)
             return -1;
         
-        
-        
         return Math.min(swapsT, swapsB);
-        // return 0;
+ 
     }
     
     private int makeEqual(int[] source, int[] dest, int k) {
@@ -41,7 +34,7 @@ class Solution {
                 continue;
             }
             else 
-                return 999;
+                return 999; //impossible to make arr values to k. So i return 999
         }
    
         return swap;
@@ -49,11 +42,8 @@ class Solution {
     }
     private int maxFreq_StoreFreq(HashMap<Integer, Integer> map, int[] arr) {
         for(int i: arr) {
-          //  System.out.print(i+" ");
             map.put(i, map.getOrDefault(i, 0)+1);
         }
-      //  System.out.println();
-        //find maxfreq element in map
         int maxi = Integer.MIN_VALUE;
         int maxElement = 0;
         for(Map.Entry<Integer, Integer> e: map.entrySet()) {
