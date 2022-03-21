60c074446833bba324cad3c97cad6459375b1c35
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-21 11:13:00 +0530

    Time: 4 ms (84.90%), Space: 43 MB (32.77%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -13,13 +13,10 @@ class Solution {
             end = Math.max(end, lastSeen[s.charAt(i) - 'a']);
             if(i==end) {
                 ans.add(end - start + 1);
-                words.add(s.substring(start, i+1));
-                start = i + 1;
+                start = end + 1;
             }
             i++;
         } 
-        for(String str: words)
-            System.out.println(str);
         return ans;
     }
 }
