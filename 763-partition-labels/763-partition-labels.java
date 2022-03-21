61002bc9866bfee4f87f2b3e356a93ecdaa9bb56
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-21 11:03:27 +0530

    Time: 9 ms (34.97%), Space: 41.4 MB (74.68%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] lastSeen = new int[26];
-        List<String> word = new ArrayList<>();
         for(int i=0;i<s.length();i++) {
             char c = s.charAt(i);
             lastSeen[c - 'a'] = i;
         }
+        int i = 0;
         int start = 0, end = 0;
         List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<s.length();i++) {
-             end = Math.max(end, lastSeen[s.charAt(i) - 'a']);
-             if(i==end) {
-                 ans.add(end-start+1);
-                 start = end+1;
-             }
+        while(i<s.length()) {
+            end = Math.max(end, lastSeen[s.charAt(i) - 'a']);
+            if(i==end) {
+                ans.add(end - start + 1);
+                start = i + 1;
+            }
+            i++;
               
         } 
         return ans;
