f851182b059cda2ac2f3a29e0c4d8cbcdbc9724e
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-25 22:38:53 +0530

    Time: 1 ms (15.10%), Space: 40.9 MB (46.68%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,15 +1,21 @@
 class Solution {
     public int numTrees(int n) {
-        return nthCatNum(n);
+        return nthCatNum(n, new HashMap<Integer, Integer>());
     }
-    private int nthCatNum(int n) {
+    private int nthCatNum(int n, HashMap<Integer, Integer> memo) {
         if(n==0 || n==1) 
             return 1;
+        int curKey = n;
+        if(memo.containsKey(curKey))
+            return memo.get(curKey);
         
         int ways = 0;
         for(int i=0;i<n;i++)
-            ways = ways + nthCatNum(i) * nthCatNum(n-i-1);
+        {
+           ways = ways + nthCatNum(i, memo) * nthCatNum(n-i-1, memo); 
+        }
+        memo.put(curKey, ways);
         
-        return ways;
+        return memo.get(curKey);
     }
 }
