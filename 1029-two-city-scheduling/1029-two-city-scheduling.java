1a3610c77136513a5075c974f55189f74a902e06
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-25 14:09:36 +0530

    Time: 1 ms (99.54%), Space: 40.1 MB (89.60%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, new custom_comp());
+        int n = costs.length;
+        int mincost = 0;
+        for(int i=0;i<n/2;i++) {
+            mincost += costs[i][0]; //add 0 to len/2 as to mincost; indicates A
+        }
+        for(int i=n/2;i<n;i++) {
+            mincost += costs[i][1]; //add next n/2 to n to mincost; indicates B
+        }
+        return mincost;
+    }
+    
+    class custom_comp implements Comparator<int[]> {
+        //to sort in ascending order based on diff [0][0] - [1][1]
+        //taking two arrays p1, p2 :
+        public int compare(int[] p1, int[] p2) {
+            return (p1[0] - p2[0] ) - (p1[1] - p2[1]);
+        }
+    }
+}
