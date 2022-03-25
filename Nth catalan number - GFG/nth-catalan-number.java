ffe73ca6ae2b7d1f43570d0f9ae6be881c699bb6
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-25 21:41:38 +0530

    Added solution - LeetHub

---
+++ Nth catalan number - GFG/nth-catalan-number.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.math.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //taking number of elements
+		    int n=sc.nextInt();
+		   
+		    //calling findCatalan() method
+		    System.out.println(new Solution().findCatalan(n));
+		}
+	}
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    public BigInteger findCatalan(int n)
+    {
+        return nthCatalan(n,new HashMap<Integer, BigInteger>());
+    }
+    public BigInteger nthCatalan(int n, HashMap<Integer, BigInteger> memo){
+        if(n <= 1){
+            return BigInteger.ONE;
+        }
+        
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        
+        BigInteger num = BigInteger.ZERO;
+        
+        for(int i = 0; i < n; i++){
+            num = num.add(nthCatalan(i, memo).multiply(nthCatalan(n -i -1, memo)));
+        }
+        
+        memo.put(n, num);
+        return memo.get(n);
+    }
+}
