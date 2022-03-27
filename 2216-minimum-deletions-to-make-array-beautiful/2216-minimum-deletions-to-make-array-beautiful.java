fd0ba2ed6a06656025a5dd02a07613204f931258
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-27 18:02:29 +0530

    Time: 7 ms (50.00%), Space: 98.8 MB (50.00%) - LeetHub

---
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minDeletion(int[] nums) {
+        int d = 0;
+        int n = nums.length;
+        for(int i=0;i<n-1;i++) {
+            int newIndex = i-d;
+            if(newIndex%2==0 && nums[i]==nums[i+1])
+                d++;
+        }
+        if((n-d)%2==0)
+            return d;
+        
+        return d+1;
+    }
+}
