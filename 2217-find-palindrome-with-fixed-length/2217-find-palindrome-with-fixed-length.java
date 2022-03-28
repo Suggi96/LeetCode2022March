5787c84b1b97c1f9f9db58bf3ae9f3170ad0b401
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 17:48:47 +0530

    Time: 179 ms (20.00%), Space: 130 MB (20.00%) - LeetHub

---
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long[] kthPalindrome(int[] queries, int intLength) {
+        int n = queries.length;
+        long[] ans = new long[n];
+        int i = 0;
+        int halfPalLen = (intLength+1)/2; //case to handle both odd and even len
+        long l = (long)(Math.pow(10, halfPalLen-1)); //10
+        long r = (long)(Math.pow(10, halfPalLen)) - 1; // 99
+        
+        for(int q: queries) {
+            long totalCount = r - l + 1;
+            if(q<=totalCount) {
+                String leftHalf = String.valueOf(l + q  - 1);
+                
+                String rightHalf = (new                                                StringBuilder(leftHalf)).reverse().toString();
+                
+                 //now for intLength 4 we have (1001) and 3 we have (1001)
+				//we don't need middle value (right half 0 index) if intLength is odd
+				ans[i]=Long.parseLong( leftHalf+rightHalf.substring(intLength % 2 ) );
+            }
+            else{
+                ans[i] = -1;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
