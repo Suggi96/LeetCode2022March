00705a139131efb488f24bc5a35a95c9b8bb8904
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 15:03:19 +0530

    Time: 1 ms (81.31%), Space: 43.7 MB (52.83%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -9,21 +9,22 @@ class Solution {
             //check if left half is sorted
             if(nums[low]<nums[mid]) {
                 //target is out of the left part boundary scope
-                if(target <nums[low] || target > nums[mid])
+                if(target < nums[low] || target > nums[mid])
                    low = mid + 1;
                 else
-                    high = mid - 1;
+                    high = mid - 1; //target lies exactly left boundary
             }
             //if this is true then right half is                                                 sorted
             else if(nums[low]>nums[mid]) {
+                //target is out of the right part boundary scope
                 if(target < nums[mid] || target > nums[high])
                     high = mid -1;
                 else 
-                    low = mid + 1;
+                    low = mid + 1; //target lies exactly in right boundary
             }
             //case if nums[mid]==nums[low] // duplicate handling 
             else {
-                low++;
+                low++; //1111111222
             }
 
         }
