ad54f1389f67a1b1a7fe60365183594d75c2066f
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 23:09:49 +0530

    Time: 13 ms (19.83%), Space: 58.9 MB (22.96%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,40 +1,38 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        
-        int maximum = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int area = 0;
+        int dp[][] = new int[m][n];
         HashMap<String, Integer> memo = new HashMap<>();
-        
-         for( int i = 0;i<matrix.length;i++){
-              for( int j = 0;j<matrix[0].length;j++){
-                  
-             if(matrix[i][j] == '1'){
-             int result =  findMaxSquare(matrix, i, j, matrix.length, matrix[0].length,memo);
-              maximum = Math.max(maximum, result * result );
-             }
-                  
-             }
-         }
-        return maximum;
-        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j]=='1') {
+                    int side = maxLength(matrix, i, j, m, n, dp);
+                    area = Math.max(area, side*side);
+                }
+            }
+        }
+        return area;
     }
-    
-    private int findMaxSquare(char[][] matrix, int currRow, int currCol, int row, int col, HashMap<String, Integer> memo)
-    {
-        if( currCol<=-1 || currCol>=col || currRow<=-1 || currRow>=row || matrix[currRow][currCol] == '0') return 0;
+    private int maxLength(char[][] matrix, int curRow, int curCol, int m, int n, 
+                         int[][] dp) {
+        //base case
+        if(curRow<0 || curRow>=m || curCol<0 || curCol>=n ||
+          matrix[curRow][curCol]=='0') //if u enter out of bounds cell or cell with value 0
+            return 0;
         
-     
-         String key = currRow + "-" + currCol;
+        if(dp[curRow][curCol]!=0)
+            return dp[curRow][curCol];
         
-        if(memo.containsKey(key)) return memo.get(key);
+        int rightExpand = 1 + maxLength(matrix, curRow, curCol+1, m, n, dp);
+        int downExpand = 1 + maxLength(matrix, curRow+1, curCol, m, n, dp);
+        int diagExpand = 1 + maxLength(matrix, curRow+1, curCol+1, m, n, dp);
         
-       int right = 1 + findMaxSquare(matrix, currRow, currCol + 1,row, col,memo);
-       int down =  1 +   findMaxSquare(matrix,currRow+1, currCol ,row, col,memo);
-       int diagonal = 1 +  findMaxSquare(matrix, currRow+1, currCol + 1, row, col,memo);
-           
-        int ans = Math.min(right, Math.min(down,diagonal));
+        int sidetoAdd = Math.min(rightExpand, Math.min(downExpand, diagExpand));
         
-        memo.put(key, ans);
+        dp[curRow][curCol] = sidetoAdd;
         
-        return memo.get(key);
+        return sidetoAdd;
     }
 }
