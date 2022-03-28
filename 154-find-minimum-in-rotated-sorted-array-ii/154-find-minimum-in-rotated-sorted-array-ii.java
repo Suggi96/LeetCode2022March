6ba683eeb6d3c4244678997f1b6621674ffaf58e
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 19:14:23 +0530

    Time: 1 ms (75.14%), Space: 43.4 MB (63.24%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.java
@@ -5,14 +5,14 @@ class Solution {
         int high = n-1;
         while(low<high) {
             int mid = low + (high-low)/2;
-            if(nums[high]==nums[mid]) {
+            if(nums[mid]==nums[high]) {
                 high--;
             }
-            else if(nums[mid] > nums[high]) {
+            else if(nums[mid] > nums[high]) { //rotation has happened. so eliminate left half
                 low = mid + 1;
             }
-            else {
-                high = mid;
+            else { //case for nums[mid] < nums[high]
+                high = mid;  //min is present in left half. so eliminate right half
             }
         }
         return nums[low];
