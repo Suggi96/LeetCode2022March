27e41089421390f387134f6cc6da020e12a421a7
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 14:55:11 +0530

    Time: 1 ms (81.31%), Space: 44 MB (37.77%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i: nums)
-            if(i==target)
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(target==nums[mid])
                 return true;
-        return false;
-    }
+            //check if left half is sorted
+            if(nums[low]<nums[mid]) {
+                //target is out of the left part boundary scope
+                if(target <nums[low] || target > nums[mid])
+                   low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            //if this is true then right half is                                                 sorted
+            else if(nums[low]>nums[mid]) {
+                if(target < nums[mid] || target > nums[high])
+                    high = mid -1;
+                else 
+                    low = mid + 1;
+            }
+            //case if nums[mid]==nums[low] // duplicate handling 
+            else {
+                low++;
+            }
+
+        }
+        return  false;
+    }  
 }
