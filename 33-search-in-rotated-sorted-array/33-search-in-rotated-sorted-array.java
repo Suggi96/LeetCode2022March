ec0d422ce57cf4d956b6fe7568e8df053c455883
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-28 12:39:46 +0530

    Time: 1 ms (68.63%), Space: 42.7 MB (40.00%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, r = n-1;
+        //find index of smallest index in nums
+        //call bs from 0 to idx-1 and bs from idx+1 to n-1 according to target
+        while(l<r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid]>nums[r])
+                l = mid +1;
+            else
+                r = mid;
+        }
+        // when loop break l will have index of min element
+        int minIdx = l;
+        if(target<=nums[n-1])
+           return binarySearch(nums, target, minIdx, n-1);
+        else
+          return binarySearch(nums, target, 0, minIdx-1);
+    }
+    private int binarySearch(int[] nums, int target, int l, int r) {
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+            if(target==nums[mid])
+                return mid;
+            else if(target<nums[mid])
+                r = mid - 1;
+            else 
+                l = mid + 1;
+        }
+        return -1;
+    }
+}
