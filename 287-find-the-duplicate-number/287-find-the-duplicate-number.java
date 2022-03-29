dc28fdd6cfb038d6ae4ca653edddc178d9ea69a5
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-29 09:58:05 +0530

    Time: 4 ms (94.53%), Space: 59.7 MB (88.60%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -2,10 +2,11 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
+        //initially slow and fast point same nums[0]. so to enter loop i use do while
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }while(slow!=fast);
+        }while(slow!=fast); //doing this because loop should run atleast once
         
         slow = nums[0];
         while(slow!=fast) {
