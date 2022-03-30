f684e4f91c1be91087ef9ec3b59c6173a6dccc41
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-30 10:39:22 +0530

    Time: 1 ms (32.47%), Space: 42.6 MB (60.33%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -8,13 +8,13 @@ class Solution {
             if(target>=matrix[i][0] && target<=matrix[i][cols-1])
                 rowNum = i;
         }
-        //apply binary search in the rowNum of matrix;
         int arr[] = new int[cols];
         int idx = 0;
         for(int i=0;i<cols;i++) {
             arr[i] = matrix[rowNum][idx];
             idx++;
         }
+          //apply binary search in the rowNum of matrix;
         return binarySearch(arr, target);
     }
     private boolean binarySearch(int[] arr, int target) {
