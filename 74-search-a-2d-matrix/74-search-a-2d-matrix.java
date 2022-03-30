3daa8e81a1e1e24e1fa4588724f658650d1d7360
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-30 10:38:42 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (9.52%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int rowNum = 0;
+        //find rowNum in which target might be present using single for loop
+        for(int i=0;i<rows;i++) {
+            if(target>=matrix[i][0] && target<=matrix[i][cols-1])
+                rowNum = i;
+        }
+        //apply binary search in the rowNum of matrix;
+        int arr[] = new int[cols];
+        int idx = 0;
+        for(int i=0;i<cols;i++) {
+            arr[i] = matrix[rowNum][idx];
+            idx++;
+        }
+        return binarySearch(arr, target);
+    }
+    private boolean binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(target==arr[mid])
+                return true;
+            else if(target>arr[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    } 
+}
