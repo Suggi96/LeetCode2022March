eb87d804bb0378f628161a7c6afc66435678aa5c
Sugandh M <44501649+Suggi96@users.noreply.github.com>
2022-03-30 12:19:15 +0530

    Time: 40 ms (78.89%), Space: 47.5 MB (81.22%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ListNode cur = head;
+        int len = 0;
+        while(cur!=null) {
+            cur = cur.next;
+            len++;
+        }
+        int ans[] = new int[len];
+        int idx = 0;
+        cur = head;
+        Stack<Pair<Integer, Integer>> st = new Stack<>();
+        while(cur!=null) {
+            if(st.empty() || st.peek().getKey()>=cur.val) {
+                st.push(new Pair<Integer, Integer>(cur.val, idx++));
+            }
+            else {
+                while(!st.empty() && st.peek().getKey()<cur.val) {
+                    int i = st.peek().getValue();
+                    ans[i] = cur.val;
+                    st.pop();
+                }
+                st.push(new Pair<Integer, Integer>(cur.val, idx++));
+            }
+            
+            cur = cur.next;
+        }
+        return ans;
+    }
+    private void printStack(Stack<Integer> st) {
+        for(int i: st)
+            System.out.print(i+" ");
+        System.out.println();
+    }
+}
